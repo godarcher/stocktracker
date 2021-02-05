c6312c3c6ce1adf6380d8a66ed4abc380c4aeec0
Joost Grunwald <joostgrunwald2001@gmail.com>
2021-02-05 10:24:39 +0100

    fetching url

--- cryptotracker.py
+++ cryptotracker.py
@@ -13,4 +13,11 @@ from win10toast import ToastNotifier
 #FUNCTIONS#
 ###########
 
-def price_finder(coin):
+def price_finder(crypto_kind):
+    #we create a google query url with the crypto input
+    gurl =  "https://www.google.com/search?q=" + crypto_kind + "+price"
+
+    #we fetch in html
+    HTML = requests.get(gurl)
+
+    
