5b4bf116f4fc4da6b31aac9043ef1f9ba0e91b9e
Joost Grunwald <joostgrunwald2001@gmail.com>
2021-02-05 10:21:59 +0100

    initial version

---
+++ cryptotracker.py
@@ -0,0 +1,16 @@
+##############
+#DEPENDENCIES#
+##############
+
+import time
+import requests
+import winsound
+
+from bs4 import BeautifulSoup
+from win10toast import ToastNotifier
+
+###########
+#FUNCTIONS#
+###########
+
+def price_finder(coin):
